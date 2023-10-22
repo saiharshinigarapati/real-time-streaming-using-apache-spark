8b57432c8400818db4f3a2b06ac9bca96088fb8e
saiharshinigarapati <48383338+saiharshinigarapati@users.noreply.github.com>
2023-10-22 15:39:18 -0500

    Add files via upload

---
+++ TwitterStreaming/src/app/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Include information necessary for Twitter API authentication
+access_token = '1677816095959404545-3GW9GClnwZibcwb3lIL1dVNcxVXc98'
+access_token_secret = '4vJgXhwY4p4mLhYXdJer2dzDHdK5NMRlIRNX0D5ILpRI2'
+consumer_key = 'ZmI2d0ZzVkpwdWJwWnhhbmRraEE6MTpjaQ'
+consumer_secret = 'McixfLZzWrbSxeGNVLeX7y6sG8hT-yOmIX3M4NO2B2fHEsMPJQ'
